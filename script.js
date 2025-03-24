733ea353f46cb798339387878f83a8829651f968
Vega <125098430+veganico98@users.noreply.github.com>
2025-03-23 22:58:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// metodo 1
+function teste(){
+    console.log($('#exemplo'))
+}
+
+$(teste)
+
+// metodo 2
+// $(document).ready(function(){
+//     function teste(){
+//         console.log($('#exemplo'))
+//     }
+
+//     teste()
+// })
